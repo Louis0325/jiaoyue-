c40ca8198c68f5a453fbf26693bf9a66f4a96255
Louis0325 <yuminlouis0325@gmail.com>
2025-12-20 22:03:48 +0800

    Add files via upload

---
+++ js/lang-en.js
@@ -0,0 +1,26 @@
+window.translations = window.translations || {};
+window.translations.en = {
+    nav_about: "About", nav_works: "Works", nav_social: "Social", nav_advertise: "Advertise", nav_contact: "Contact",
+    hero_greeting: "Hi, I am",
+    hero_sub: "Digital Creator / Shiba Inu Furry / Life Art Explorer",
+    hero_intro_title: "Profile",
+    hero_intro_content: "I'm Jiao Yue, a furry who likes to bite. If I can't bite others, I bite myself😅",
+    furry_title: "My Fursona",
+    furry_label_name: "Name:", furry_val_name: "Jiao Yue",
+    furry_label_species: "Species:", furry_val_species: "Shiba Inu",
+    furry_label_features: "Features:", furry_val_features: "Warm grey fur and tail.",
+    furry_label_personality: "Personality:", furry_val_personality: "Curious, helpful, but very shy.",
+    birthday_title: "Birthday Countdown",
+    birthday_label: "Birthday Date",
+    time_days: "Days", time_hours: "Hrs", time_minutes: "Min", time_seconds: "Sec",
+    title_works: "Featured Works", title_social: "Social Media",
+    work1_title: "YouTube Videos", work1_desc: "A collection of all my video works on YouTube.", work1_btn: "Go to YouTube Channel",
+    work2_title: "Digital Art", work2_desc: "Showcasing my recent digital drawings.", work2_btn: "View on PENUP",
+    work3_title: "Other Works", work3_desc: "Showcasing other digital art pieces.", work3_btn: "View on PENUP",
+    title_advertise: "Advertising",
+    advertiser1_name: "Asteroid Host", advertiser1_desc: "Free hosting service", advertiser1_btn: "Visit Website",
+    advertiser2_name: "DiamondHost", advertiser2_desc: "Top choice for Minecraft server hosting. Use code '鑽鑽' for exclusive discounts!", advertiser2_btn: "Visit Website",
+    advertiser3_name: "CoffeeHost", advertiser3_desc: "Top choice for Minecraft Bedrock server hosting.", advertiser3_btn: "Visit Website",
+    footer_slogan: "© 2024 Jiao Yue. Lighting up the web with creativity.",
+    lang_choice: "Language", current_lang: "English"
+};
